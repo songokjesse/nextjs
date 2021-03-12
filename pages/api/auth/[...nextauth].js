e827cd375427b7294e6deda83f531b446674fd58
jessesongok <jessesongok@gmail.com>
2021-03-12 14:48:41 +0300

    Fixed dependecy issue (next-auth adapters)

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers  from 'next-auth/providers'
 import { PrismaClient } from '@prisma/client'
+import Adapters from 'next-auth/adapters'
 
 const prisma = new PrismaClient()
 
