df8156e12bdab77eaed3eed806244322c95809ed
jessesongok <jessesongok@gmail.com>
2021-03-12 21:29:22 +0300

    added userlist getprops

--- components/users/UserList.js
+++ components/users/UserList.js
@@ -1,7 +1,18 @@
-const UserList = () => {
+import prisma from '../../lib/dbConnection'
+export const getStaticProps = async () => {
+    const response = await prisma.users.findMany()
+    const users = response.json()
+      return { props: { users } }
+}
+
+const UserList = ({users}) => {
     return (
         <div>
-            UserList
+             {users.map((user) => (
+            <div key={user.id} className="post">
+                {user.email}
+            </div>
+          ))}
         </div>
     )
 }
--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,10 +1,14 @@
+import {useSession} from 'next-auth/client'
 import UserList from "../../../components/users/UserList"
 
 const UserHome = () => {
+    const [ session, loading ] = useSession()
+    // if (loading) return null
+    // if (!loading && !session) return <p>Access Denied</p>
     return (<div>
         UserHome
         <hr/>
-        <UserList/>
+        <UserList />
 
     </div>)
 }
