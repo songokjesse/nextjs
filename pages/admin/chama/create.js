881c0980f6adc60c99b8a369f18f638cd479a3fb
jessesongok <jessesongok@gmail.com>
2021-04-06 19:12:58 +0300

    disabled prisma code

--- pages/admin/chama/create.js
+++ pages/admin/chama/create.js
@@ -7,15 +7,13 @@ const Create_Chama = () => {
                 <div>
                     <label className="block text-sm font-medium text-gray-700">Price</label>
                     <div className="mt-1 relative rounded-md shadow-sm">
-                    <input name="name" placeholder="Chama Name" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" />
+                    <input name="name" placeholder="Chama Name" classNamw="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md" />
                     </div>
                 </div>
                 <label className="block text-sm font-medium text-gray-700">Chama Description</label>
                 <div className="mt-1 relative rounded-md shadow-sm">
-                <textarea name="description" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Add description here" />
+                <textarea name="description" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md" placeholder="Add description here" />
                 </div>
-                <br/>
-                <br/>
                 <br/>
                 <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded"> Create</button>
             </form>
--- pages/admin/chama/index.js
+++ pages/admin/chama/index.js
@@ -1,18 +1,18 @@
-import prisma from '../../../lib/dbConnection'
+// import prisma from '../../../lib/dbConnection'
 // export const getStaticProps = async () => {
 //     const chamas  = await prisma.chamas.findMany()
 //     return { props: chamas}
 // }
 
-const Chama = (chamas) => {
+const Chama = () => {
     return (
     <div>
         <h1>Chama List </h1>
-        {chamas.map((chama) => {
-            <div >
+        {/*{chamas.map((chama) => {*/}
+        {/*    <div >*/}
 
-            </div>
-        })}
+        {/*    </div>*/}
+        {/*})}*/}
     </div>
     )
 }
